4a970b24642de530f404ed03dc927463ab5082bc
DaFei <deFang@users.noreply.github.com>
2022-08-05 14:45:06 +0800

    deploy to bsc

--- test/03.MegaSwapper.paraswap.test.js
+++ test/03.MegaSwapper.paraswap.test.js
@@ -95,20 +95,25 @@ describe('MegaSwapper', function () {
         alice.name = 'alice'
         bob.name = 'bob'
 
-        chainId = 1
-        eth_address = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
-        usdc_address = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-        weth_address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-        dai_address = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
-        toke_address = "0x2e9d63788249371f1DFC918a52f8d799F4a38C94"
-        wbtc_address = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"
-        impersonateAddress = "0xa205fd7344656c72fdc645b72faf5a3de0b3e825"
+        // chainId = 1
+        // eth_address = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+        // usdc_address = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+        // weth_address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+        // dai_address = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+        // wbtc_address = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"
+        // impersonateAddress = "0xa205fd7344656c72fdc645b72faf5a3de0b3e825"
 
+        chainId = 56
+        eth_address = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+        usdc_address = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"
+        weth_address = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c" // wbnb
+        dai_address = "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3"
+        wbtc_address = "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c"
+        impersonateAddress = "0xe2fc31f816a9b94326492132018c3aecc4a93ae1"
 
         usdc = await ethers.getContractAt('IERC20', usdc_address)
         weth = await ethers.getContractAt('IERC20', weth_address)
         dai = await ethers.getContractAt('IERC20', dai_address)
-        toke = await ethers.getContractAt('IERC20', toke_address)
         wbtc = await ethers.getContractAt('IERC20', wbtc_address)
 
         await hre.network.provider.request({
@@ -128,11 +133,12 @@ describe('MegaSwapper', function () {
         megaswapper = await (await ethers.getContractFactory('MegaSwapper')).deploy()
         await usdc.connect(signer).approve(megaswapper.address, MAX)
         console.log('before btc balance', (await wbtc.balanceOf(signer.address))/1e8)
-        txdata = await getTxdata(usdc_address, 6, wbtc_address, 8, decimalStrUSDT("1000"), 1, megaswapper.address, signer)
+        // txdata = await getTxdata(usdc_address, 6, wbtc_address, 8, decimalStrUSDT("1000"), 1, megaswapper.address, signer)
+        txdata = await getTxdata(usdc_address, 18, wbtc_address, 18, decimalStr("1000"), chainId, megaswapper.address, signer)
         executor_address = txdata.to
         executor_data = txdata.data
         console.log("executor_address", executor_address, "executor_data", executor_data)
-        await megaswapper.connect(signer).swap(usdc_address, wbtc_address, decimalStrUSDT("1000"), true, executor_address, executor_data)
+        await megaswapper.connect(signer).swap(usdc_address, wbtc_address, decimalStr("1000"), true, executor_address, executor_data)
         console.log('after btc balance', (await wbtc.balanceOf(signer.address))/1e8)
 
     })
